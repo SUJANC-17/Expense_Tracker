fb4e760398b72fab0bad95949d446ab7f4054559
Sujan C <sujanc3306@gmail.com>
2026-05-28 16:08:55 +0530

    chore: remove serveo from start_all.bat to run exclusively on localhost

--- start_all.bat
+++ start_all.bat
@@ -1,10 +1,5 @@
 @echo off
-echo Starting Expense Tracker Project...
-
-IF NOT EXIST "%USERPROFILE%\.ssh\id_rsa" (
-    echo Generating SSH key for Serveo...
-    ssh-keygen -t rsa -N "" -f "%USERPROFILE%\.ssh\id_rsa"
-)
+echo Starting Expense Tracker Project Locally...
 
 echo 1. Starting Backend Server...
 start "Backend Server" cmd /k "cd server && if not exist node_modules (echo Installing server dependencies... && call npm install) && npm run dev"
@@ -12,9 +7,6 @@ start "Backend Server" cmd /k "cd server && if not exist node_modules (echo Inst
 echo 2. Starting Frontend Client...
 start "Frontend Client" cmd /k "cd client && if not exist node_modules (echo Installing client dependencies... && call npm install) && npm run dev"
 
-echo 3. Starting Serveo Tunnel...
-start "Serveo Tunnel" cmd /k "echo Waiting for frontend to start... && timeout /t 5 >nul && echo Starting tunnel... && ssh -o StrictHostKeyChecking=accept-new -o ServerAliveInterval=60 -R expense:80:localhost:5173 serveo.net"
-
 echo All services started! Check the new windows.
-echo Public URL: https://expense.serve.com
+echo Local URL: http://localhost:5173
 pause
